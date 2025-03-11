9b9ec5974e6c920656fd9b1e4ff3f9cae0d4a89c
jskpaul <paulkim2025@u.northwestern.edu>
2025-03-10 20:56:25 -0500

    no hardcoding url

--- src/common/components/OrderTable.jsx
+++ src/common/components/OrderTable.jsx
@@ -384,7 +384,7 @@ export default function OrderTable() {
     };
   }, []);
   useEffect(() => {
-    fetch('http://localhost:5050/orders/')
+    fetch(`${process.env.REACT_APP_BACKEND_URL}/orders/`)
       .then((result) => result.json())
       .then((data) => {
         // tranform each order of JSON into flat object for table
